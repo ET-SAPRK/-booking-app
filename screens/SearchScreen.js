503e6e928d13953526d42cdb6147ca2607cd4303
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-22 20:20:51 +0300

    edited

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,11 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
+import React ,{useEffect, useState} from "react";
+import { Feather } from "@expo/vector-icons";
+import SearchResults from "../components/SearchResults";
 
 const SearchScreen = () => {
+  const [input,setInput] = useState("");
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View
+        style={{
+          padding: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderColor:"#FFC72C",
+          borderWidth:4,
+          borderRadius:10,
+          marginTop:30,
+          marginLeft:10,
+          marginRight:10,
+          marginBottom:10,
+        }}
+      >
+         <TextInput value={input} onChangeText={(text) => setInput(text)} placeholder="Enter Your Destination" />
+         <Feather name="search" size={22} color="black" />
+      </View>
+      {/* <SearchResults data={data} input={input} setInput={setInput}/> */}
+    </SafeAreaView>
   )
 }
 
